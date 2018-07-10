d77aeeb9f09f68845e0d57e6e2766c265dfa27fd
Trevor Jameson <tjameson15@gmail.com>
2018-07-10 11:56:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,10 @@ def oxford_comma(array)
     return array[0].to_s
   elsif array.size == 2
     return array.join(" and ")
-  elsif array.size > 1
+  elsif array.size > 2
   array[-1] = "and " + array[-1]
-  output_arr = array.join(", ")
-  puts output_arr
+  array = array.join(", ")
+  puts array
 end
 end
 
