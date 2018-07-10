9482959eb293c214ef2fd198f1475e7dd9f8aaa5
Trevor Jameson <tjameson15@gmail.com>
2018-07-10 11:53:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
   if array.size == 1
     return array.to_s
+  elsif array.size > 1
   array[-1] = "and " + array[-1]
   output_arr = array.join(", ")
   puts output_arr
+end
 end
 
 
