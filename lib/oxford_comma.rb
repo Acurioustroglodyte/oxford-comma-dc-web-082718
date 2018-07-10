d8245d778c2611785002677c6b2262f8a8db89e8
Trevor Jameson <tjameson15@gmail.com>
2018-07-10 11:59:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0].to_s
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.size > 2
+  array[-1] = "and " + array[-1]
+  joined_str = array.join(", ")
+  return joined_str
 end
+end
+
+
+oxford_comma(["amy", "bill", "charly"])
